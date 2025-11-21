8896ce400e92efbc73673d5c63a1a686a0220b25
Escalante Antonio <antonio.escalante@mxc.mcd.com>
2025-11-20 23:57:34 -0600

    SANDBOX-02-201125 feat: Add Terraform configuration for an EC2 instance with a simple HTTP server

---
+++ sandbox/02/deploying-a-web-server/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.99.1"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-2"
+  profile = "default"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0fb653ca2d3203ac1"
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+      #!/bin/bash
+      echo "Hello, world" > index.html
+      nohup busybox httpd -f -p 8080 &
+      EOF
+
+  user_data_replace_on_change = true
+
+  tags = {
+      Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
